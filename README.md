47fef7a31e6a1269d1520fabe7d1f66fd03dd34e
Peter Bishop <pjb.den@gmail.com>
2024-12-21 13:15:07 -0700

    basic server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # symmetrical-octo-broccoli
+
+Map Annotations
+Image Uploading
+Text input with formatting
+TextField validation
---
+++ server/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const cors = require('cors');
+const app = express();
+const http = require('http'); // Import HTTP module to work with Socket.IO
+const { Server } = require('socket.io');
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const allowedOrigins = [
+    /^http:\/\/localhost(:\d+)?$/, //localhost:allports
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        callback(null, true);
+        } else if (allowedOrigins.some(o => typeof o === 'string' ? o === origin : o.test(origin))) {
+        callback(null, true);
+        } else {
+        callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Symmetrical Server!');
+  });
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+  },
+});
+
+const configureSocketIO = (io) => {
+    io.on('connection', (socket) => {
+      console.log('A user connected on port:', PORT);
+  
+      socket.on('fromSwiftUI', (data) => {
+        console.log(`Message received on port ${PORT}:`, data);
+      });
+
+      socket.on('fromReact', (data) => {
+        console.log(`Message received on port ${PORT}:`, data);
+      });
+  
+      socket.on('FireAuth', (data) => {
+        console.log(`Message received on port ${PORT}:`, data);
+      });
+  
+      socket.on('disconnect', () => {
+        console.log(`User disconnected from port ${PORT}`);
+      });
+    });
+  };
+
+configureSocketIO(io); 
+
+server.listen(PORT, () => {
+    console.log(`HTTP server and Socket.IO listening on http://localhost:${PORT}`);
+  });
